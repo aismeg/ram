3cfecfdda51d36acb57222ab793f0e72edcf7f4c
aismeg <0>
2023-08-04 20:03:21 +0300

    ongoing, responsive - mobile

--- src/Components/SIdebar/Filter/Type.js
+++ src/Components/SIdebar/Filter/Type.js
@@ -4,7 +4,7 @@ export const Type = ({ setType, setPage }) => {
 
     return (<section className="filter filter-type">
         <div className="search-bar type-bar">
-            <input type="search" onChange={(e) => {
+            <input className="type-input" type="search" onChange={(e) => {
                 setType(e.target.value);
                 setPage(1);
             }} placeholder="Search for a type" />
